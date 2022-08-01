5dc9b779258c66508292402d2ea354e7ad1f7849
JUL1VER <jul1v3r@gmail.com>
2022-08-01 15:57:21 +0300

    text from inputs to console

--- src/components/contactmeBlock/ContactmeBlock.js
+++ src/components/contactmeBlock/ContactmeBlock.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import contactmeImg from '../../img/contactmeImg.png'
 import contactmeImg_large from '../../img/contactmeImg_large.png'
 import paperplane from '../../img/paperplane.png'
@@ -6,6 +6,17 @@ import paperplane_large from '../../img/paperplane_large.png'
 import './contactmeBlock.css';
 
 const ContactmeBlock = () => {
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    const sendInfo = () => {
+        const info = {
+            'email': email,
+            'message': message
+        }
+        console.log(info);
+    }
+
     return (
         <div className='ContactmeBlock_wrapper'>
             <h3 className='ContactmeBlock_header'>Contact me</h3>
@@ -18,9 +29,20 @@ const ContactmeBlock = () => {
                     <img src={contactmeImg} alt="ContactmeImg" className='ContactmeBlock_img'/>
                 </picture>
                 <form className='ContactmeBlock_form'>
-                    <input type="email" placeholder='Enter email address' className='ContactmeBlock_email'/>
-                    <textarea type="text" placeholder='Enter message...' className='ContactmeBlock_message'/>
-                    <button className='ContactmeBlock_button' type='button'>
+                    <input 
+                        type="email" 
+                        placeholder='Enter email address' 
+                        className='ContactmeBlock_email'
+                        onChange={(e) => setEmail(e.target.value)}/>
+                    <textarea 
+                        type="text" 
+                        placeholder='Enter message...' 
+                        className='ContactmeBlock_message'
+                        onChange={(e) => setMessage(e.target.value)}/>
+                    <button 
+                        className='ContactmeBlock_button' 
+                        type='button' 
+                        onClick={sendInfo}>
                         Send
                         <picture className="ContactmeBlock_button_imgPic">
                             <source
