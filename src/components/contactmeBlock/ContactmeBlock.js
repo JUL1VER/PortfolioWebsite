6ba45c4cdaa10d50feb2ad48e507d37df55967b6
JUL1VER <jul1v3r@gmail.com>
2022-08-02 18:41:12 +0300

    validation

--- src/components/contactmeBlock/ContactmeBlock.js
+++ src/components/contactmeBlock/ContactmeBlock.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import contactmeImg from '../../img/contactmeImg.png'
 import contactmeImg_large from '../../img/contactmeImg_large.png'
 import paperplane from '../../img/paperplane.png'
@@ -6,26 +6,41 @@ import paperplane_large from '../../img/paperplane_large.png'
 import './contactmeBlock.css';
 
 const ContactmeBlock = () => {
-    // const [email, setEmail] = useState('');
-    // const [message, setMessage] = useState('');
-
-    // const sendInfo = () => {
-    //     const info = {
-    //         'email': email,
-    //         'message': message
-    //     }
-    //     console.log(info);
-    // }
-
-    const [info, setInfo] = useState({email: '', message: ''});
-
+    const [emailDirty, setEmailDirty] = useState(false);
+    const [textDirty, setTextDirty] = useState(false);
+    const [emailError, setEmailError] = useState('Email cannot be empty');
+    const [textError, setTextError] = useState('Message cannot be empty');
+    const [info, setInfo] = useState({ email: '', message: '' });
     
+    const blurHandler = (e) => {
+        switch (e.target.name) {
+            case 'email':
+                setEmailDirty(true)
+                break
+            case 'text':
+                setTextDirty(true)
+                break
+            default: break
+        }
+    }
+
     const handleEmailChange = (e) => {
         setInfo(state => ({ ...state, email: e.target.value }))
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        if (!re.test(String(e.target.value).toLowerCase())) {
+            setEmailError('Incorrect email')
+        } else {
+            setEmailError('')
+        }
     }
 
     const handleMessageChange = (e) => {
         setInfo(state => ({ ...state, message: e.target.value }))
+        if (String(e.target.value).length < 1) {
+            setTextError('Incorrect message')
+        } else {
+            setTextError('')
+        }
     }
 
     const sendInfo = () => {
@@ -49,16 +64,24 @@ const ContactmeBlock = () => {
                     <img src={contactmeImg} alt="ContactmeImg" className='ContactmeBlock_img'/>
                 </picture>
                 <form className='ContactmeBlock_form'>
+                    {(emailDirty && emailError) && <div style={{color: 'red', margin: '0px 0px 10px 0px' }}>{emailError}</div>}
                     <input 
-                        type="email" 
+                        name='email'
+                        type='email'
                         placeholder='Enter email address' 
                         className='ContactmeBlock_email'
-                        onChange={handleEmailChange}/>
+                        value={info.email}
+                        onChange={handleEmailChange}
+                        onBlur={e => blurHandler(e)}/>
+                    {(textDirty && textError) && <div style={{color: 'red', margin: '20px 0px -10px 0px'}}>{textError}</div>}
                     <textarea 
-                        type="text" 
+                        name='text'
+                        type='text'
                         placeholder='Enter message...' 
                         className='ContactmeBlock_message'
-                        onChange={handleMessageChange}/>
+                        value={info.message}
+                        onChange={handleMessageChange}
+                        onBlur={e => blurHandler(e)}/>
                     <button 
                         className='ContactmeBlock_button' 
                         type='button' 
