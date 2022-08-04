353de0a8bcb2f07bf23302fa0295417c1e40963a
JUL1VER <jul1v3r@gmail.com>
2022-08-04 23:02:23 +0300

    how to stop fixing code?

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import './App.css';
 import Header from './components/header/Header';
 import MainPage from './components/mainPage/MainPage';
 import Footer from './components/footer/Footer';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{margin: '0 auto', width: '100%', textAlign: 'center'}}>
       <Header/>
       <MainPage/>
       <Footer/>
