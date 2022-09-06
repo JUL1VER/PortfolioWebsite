0cd582aa2dc18d40050e10bee6cc4ac86b7e205a
JUL1VER <jul1v3r@gmail.com>
2022-09-06 17:55:09 +0300

    add target to links

--- src/components/skillIcon/SkillIcon.js
+++ src/components/skillIcon/SkillIcon.js
@@ -5,7 +5,7 @@ const SkillIcon = (props) => {
     const {imgprop, src, title} = props;
     return (
         <GlitchText component='span' onHover={true} color1='rgba(212, 81, 25, 0.7)' color2='rgba(25, 100, 212, 0.7)' className='SkillsBlock_logoWithText'>
-            <a href={src} style={{textDecoration: 'none'}}>
+            <a href={src} target='_blank' rel='noreferrer' style={{textDecoration: 'none'}}>
                 <img src={imgprop} alt="skillIcon" className='SkillsBlock_logos'/>
                 <p className='SkillsBlock_logos_text'>{title}</p>
             </a>
