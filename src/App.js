a17726c4004bad298f6201c34df3f20dca6599df
JUL1VER <jul1v3r@gmail.com>
2022-09-06 17:31:28 +0300

    remove firefox background

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './app.css';
 import MainPage from './components/mainPage/MainPage';
 import MatrixBackground from './components/MatrixBackground/MatrixBackground';
+import Bowser from "bowser";
 
 function App() {
+  const browser = Bowser.getParser(window.navigator.userAgent);
   return (
-    <div className="App" style={{margin: '0 auto', width: '100%', textAlign: 'center'}}>  
-        <MatrixBackground/>
+    <div className="App" style={{margin: '0 auto', width: '100%', textAlign: 'center'}}> 
+        {browser.getBrowserName() === 'Firefox' ? null : <MatrixBackground/>}
         <MainPage/>
     </div>
   );
